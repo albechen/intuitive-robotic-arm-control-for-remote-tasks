c21510e59fac3b6dc853f64ac26e3ba3dfd3f4f4
TheWaterFell <59457095+albechen@users.noreply.github.com>
2023-01-15 21:14:31 -0500

    feat: start socket add

--- ardruino/stepper_serial_28BYJ/stepper_serial_28BYJ.ino
+++ ardruino/stepper_serial_28BYJ/stepper_serial_28BYJ.ino
@@ -4,7 +4,7 @@
 // Defines the number of steps per rotation
 const int stepsPerRevolution = 2038;
 const int delay_step = 2200;
-const int num_steppers = 3;
+const int num_steppers = 4;
 
 // Creates an instance of stepper class
 // Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
@@ -12,11 +12,14 @@ Stepper step1 = Stepper(stepsPerRevolution, 23, 27, 25, 29);
 Stepper step2 = Stepper(stepsPerRevolution, 33, 37, 35, 39);
 Stepper step3 = Stepper(stepsPerRevolution, 43, 47, 45, 49);
 Stepper step4 = Stepper(stepsPerRevolution, 22, 26, 24, 28);
-Stepper stepperList[3] = {step1, step2, step3};
+Stepper stepperList[3] = {step1, step2, step3, step4};
 
 SerialTransfer myTransfer;
-long target_steps[3] = {0, 0, 0};
-long current_steps[3] = {0, 0, 0};
+// put claw 45 degree - set program to think it's at 90
+// claw is using the same stepper but it's designed to move CW
+// for pos so dont need to change neg
+long target_steps[4] = {0, 0, 0, 510};
+long current_steps[4] = {0, 0, 0, 510};
 
 void setup()
 {
