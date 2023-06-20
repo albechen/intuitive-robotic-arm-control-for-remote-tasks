3ad432c6bd6b8868c888f3b72d78f4a9cdde3b51
albechen <59457095+albechen@users.noreply.github.com>
2023-06-20 02:46:08 -0400

    feat: update readme gif

--- conv_mp4_to_gif.py
+++ conv_mp4_to_gif.py
@@ -1,10 +1,34 @@
-from moviepy.editor import *
+# %%
+import moviepy.editor as mp
 
 
-def convert_mp4_to_gif(mp4_file, gif_file):
-    video = VideoFileClip(mp4_file)
-    video.write_gif(gif_file)
+# %%
+def convert_mp4_to_gif(vid_name, fps, fuzz, output_width):
+    mp4_path = "images/original/{}.mp4".format(vid_name)
+    gif_path = "images/{}.gif".format(vid_name)
+
+    video = mp.VideoFileClip(mp4_path)
+    width = video.size[0]
+    height = video.size[1]
+    new_ratio = output_width / width
+    new_height = new_ratio * height
+    video = video.resize(height=new_height, width=output_width)  # type: ignore
+    video.write_gif(gif_path, fps=fps, fuzz=fuzz)
 
 
-# Usage example
-convert_mp4_to_gif("input.mp4", "output.gif")
+fps = 10
+fuzz = 10
+output_width = 1500
+
+# %%
+vid_name = "integration_remote"
+convert_mp4_to_gif(vid_name, fps, fuzz, output_width)
+
+# %%
+vid_name = "hand_detection_3d"
+convert_mp4_to_gif(vid_name, fps, fuzz, output_width)
+
+# %%
+vid_name = "arm_design_version"
+convert_mp4_to_gif(vid_name, fps, fuzz, output_width)
+# %%
